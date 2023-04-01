ebc22b7b80df5a0414cb8e648bcefe7f196a6827
Nicolas Laurenzano <nlaurenzano@gmail.com>
2023-04-01 18:27:08 -0300

    Ejercicio 1

--- ejercicio1/src/app/app.component.ts
+++ ejercicio1/src/app/app.component.ts
@@ -1,9 +1,3 @@
-// 1- (app.component.html) Realizar:
-// una aplicacion que se le ingresen dos edades en dos cuadro de textos
-// edadUno, EdadDos
-// mostrar el promedio y la suma en dos cuadros de textos nuevos
-// botones "calcular" y "limpiar cuadros de textos"
-
 import { Component } from '@angular/core';
 
 @Component({
